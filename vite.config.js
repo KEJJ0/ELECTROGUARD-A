6bd7ded723fd44027973981f7983b2354fd94e09
KEJJ0 <sh8a9621@gmail.com>
2025-09-30 18:39:46 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
-
-const isProd = process.env.NODE_ENV === 'production'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: { alias: { '@': path.resolve(__dirname, './src') } },
-  base: isProd ? '/ELECTROGUARD-A/' : '/',
-    resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } }
-
+  base: '/ELECTROGUARD-A/' // غيّرها لاسم مستودعك لو مختلف
 })
